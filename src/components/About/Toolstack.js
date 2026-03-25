366cb8744346c50a8cd1ec44609acba9ae626d96
rust4work <rustambakhtiyorov4@gmail.com>
2026-03-25 18:09:19 +0900

    fixed import bugs

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,9 +1,8 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import macOs from "../../Assets/TechIcons/Apple MacOSX.svg";
+
 import chrome from "../../Assets/TechIcons/Google Chrome.svg";
 import vsCode from "../../Assets/TechIcons/vscode.svg";
-import intelliJ from "../../Assets/TechIcons/intellij-idea.svg";
 
 function Toolstack() {
   return (
